025824136078888649368357ea7cccbaded1f48d
Kruza <macik1@hotmail.com>
2020-11-27 13:40:23 +0100

    Initial generator branch

--- elephant_flow.py
+++ elephant_flow.py
@@ -3,6 +3,14 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+from flows_generator import generate_flows
+
+# GLOBAL VARIABLES
+experiment_duration = 180  # seconds
+n_mice_flows = 45
+n_elephant_flows = 5
 
 class ElephantFlowTopo(Topo):
     def build(self):
@@ -37,19 +45,45 @@ class ElephantFlowTopo(Topo):
         self.addLink( s2, m3 )
 
 
-def simpleTest():
+def Test():
     "Create and test a simple network"
     topo = ElephantFlowTopo()
     net = Mininet(topo)
     net.start()
-    print "Dumping host connections"
-    dumpNodeConnections(net.hosts)
-    print "Testing network connectivity"
-    net.pingAll()
-    CLI(net)
-    net.stop()
+
+    user_input = "QUIT"
+
+    while True:
+        # if user enters CTRL + D then treat it as quit
+        try:
+            user_input = raw_input("GEN/CLI/QUIT: ")
+        except EOFError as error:
+            user_input = "QUIT"
+
+        if user_input.upper() == "GEN":
+            experiment_duration = int(raw_input("Experiment duration: "))
+            n_elephant_flows = int(raw_input("Numb of elephant flows: "))
+            n_mice_flows = int(raw_input("Numb of mice flows: "))
+
+            generate_flows(n_elephant_flows, n_mice_flows, experiment_duration, net, log_dir)
+
+        elif user_input.upper() == "CLI":
+            info("Running CLI...\n")
+            CLI(net)
+
+        elif user_input.upper() == "QUIT":
+            info("Terminating...\n")
+            info("Dumping host connections")
+
+            dumpNodeConnections(net.hosts)
+            print "Testing network connectivity"
+            net.stop()
+            break
+
+        else:
+            print("Command not found")
 
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
-    simpleTest()
+    Test()
